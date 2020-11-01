de5ff009286f0c90cc6c8dfacfb36883b0f7d5cc
Eunbyul na Proctor <51739602+EunbyulNa@users.noreply.github.com>
2020-11-01 19:31:56 +0900

    Add files via upload

--- card.js
+++ card.js
@@ -4,12 +4,34 @@ $(document).ready(function () {
   $('.menu_btn>a').on('click', function() {
    $('#overlay_wraps').fadeIn()
  }),
+
+
+//오버레이 되면 스크롤 다운 막기//
+ $('#overlay_wraps').on('scroll touchmove mousewheel', function(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  return false;
+});
+
  $('.close_btn>a').on('click', function () {
    $('#overlay_wraps').fadeOut()
  });
 
 
+//스크롤바 위치가져오//
 
+$(window).scroll(function() {
+
+  var height = $(document).scrollTop();
+  console.log(height);
+
+   if(height > '900' && height < '2760') {
+     $('.right_contents').addClass('fixedP');
+   }else {
+     $('.right_contents').removeClass('fixedP');
+   }
+
+});
 
 
 
@@ -31,5 +53,26 @@ $(document).ready(function () {
    });
 
  },2000);
+
+
+//탭박스//
+$('.tab_box div:gt(0)').hide();
+
+var nn;
+
+$('.tab li').click(function() {
+
+  nn = $(this).index();
+
+  $('.tab li').removeClass('select');
+  $(this).addClass('select');
+  $('.tab li').removeClass('redB');
+  $(this).addClass('redB');
+
+  $('.tab_box div').hide();
+  $('.tab_box div').eq(nn).show();
+});
+
+
 
 });
